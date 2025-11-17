81daef5a49be1d499023490cb1a8d8df014ad1ff
Ahab Isaac <ahabisaac13@gmail.com>
2025-11-17 14:21:49 +0000

    change cuda version to 11.8

--- experiments/run_worker.sh
+++ experiments/run_worker.sh
@@ -17,7 +17,7 @@ module load python3/3.9-gnu-10.2.0
 module load gcc-libs/10.2.0
 
 #pytorch
-module load cuda/11.3.1/gnu-10.2.0
+module load cuda/11.8.0/gnu-10.2.0
 module load cudnn/8.2.1.32/cuda-11.3
 module load pytorch/2.1.0/gpu
 
