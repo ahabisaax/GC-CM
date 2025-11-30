2bf53afd604534ee1bbdc1c608c412e44b8a4daa
Ahab Isaac <ahabisaac13@gmail.com>
2025-11-30 21:51:40 +0000

    update loss function so ReLU

--- xai_concept_leakage/models/crcbm.py
+++ xai_concept_leakage/models/crcbm.py
@@ -727,9 +727,13 @@ class CriticRegularisedConceptBottleneckModel(pl.LightningModule):
             #TODO ReLU this and check behaviour
             concept_loss = self.loss_concept(c_sem, c)
             concept_loss_scalar = concept_loss.detach().item()
+            raw_gap = task_loss - adversarial_loss
+            hinge_penalty = torch.clamp(raw_gap, min=0)
+            adversarial_term = adversarial_loss_weight * hinge_penalty
+
             loss = (
                 self.concept_loss_weight * concept_loss
-                + task_loss + adversarial_loss)
+                + (1-adversarial_loss_weight)*task_loss + adversarial_term)
         else:
             loss = task_loss + adversarial_loss
             concept_loss_scalar = 0.0
