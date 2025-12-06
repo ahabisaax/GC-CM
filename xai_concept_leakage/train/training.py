d6d7b6b3eefd185018b27c54fac7eda37ae01c66
Ahab Isaac <ahabisaac13@gmail.com>
2025-12-06 11:34:55 +0000

    lr finder on concepts

--- xai_concept_leakage/train/training.py
+++ xai_concept_leakage/train/training.py
@@ -260,22 +260,19 @@ def train_end_to_end_model(
                 model.cbm_learning_rate = new_lr
                 model.adv_learning_rate = new_lr
 
-            if trainer.logger:
-                # 1. Generate the plot using Lightning's built-in method
-                fig = lr_finder.plot(suggest=True)
+                if trainer.logger:
+                    # 1. Generate the plot using Lightning's built-in method
+                    fig = lr_finder.plot(suggest=True)
 
-                # 2. Log the figure to W&B
-                # We access the underlying W&B run object via .experiment
-                trainer.logger.experiment.log({
-                    "lr_finder/plot": wandb.Image(fig),
-                    "lr_finder/suggestion": new_lr
-                })
+                    # 2. Log the figure to W&B
+                    # We access the underlying W&B run object via .experiment
+                    trainer.logger.experiment.log({
+                        "lr_finder/plot": wandb.Image(fig),
+                        "lr_finder/suggestion": new_lr
+                    })
 
-            # 4. Apply and Scale
-            model.adv_learning_rate = model.cbm_learning_rate = new_lr
 
-            # 5. Disable LR Find Mode for Real Training
-            model.lr_find_mode = False
+
             if activation_freq:
                 fit_trainer = utils.ActivationMonitorWrapper(
                     model=model,
