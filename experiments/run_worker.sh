f94a00652fa88859fdb9ae44fd595f2efa57989d
Ahab Isaac <ahabisaac13@gmail.com>
2025-12-17 16:43:02 +0000

    path for results dir

--- experiments/run_worker.sh
+++ experiments/run_worker.sh
@@ -30,7 +30,7 @@ export MKL_NUM_THREADS=$NSLOTS
 # --- 3. PATHS ---
 PROJECT_ROOT=~/Scratch/xai-crcbm
 #CHANGE FOR EACH DATASET
-RESULTS_DIR=$PROJECT_ROOT/results/dsprites_models
+RESULTS_DIR=$PROJECT_ROOT/results/dsprites_dep_0_models
 CONFIG_PATH=$PROJECT_ROOT/experiments/configs/dsprites.yaml
 
 export PYTHONPATH=$PROJECT_ROOT:$PYTHONPATH
