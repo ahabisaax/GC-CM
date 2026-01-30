f0c4f2adc53bd35e3f0216d5d9b14ac745387e24
Ahab Isaac <ahabisaac13@gmail.com>
2026-01-30 14:22:31 +0000

    test run for worker

--- experiments/run_cub_worker_2.sh
+++ experiments/run_cub_worker_2.sh
@@ -3,7 +3,7 @@
 #$ -o ~/Scratch/xai-crcbm/logs/CUB_$JOB_ID.out
 #$ -e ~/Scratch/xai-crcbm/logs/CUB_$JOB_ID.err
 #$ -pe smp 8
-#$ -l h_rt=17:00:00
+#$ -l h_rt=01:00:00
 #$ -l mem=24G
 #$ -l tmpfs=20G
 #$ -wd /home/ucakais/Scratch/xai-crcbm
