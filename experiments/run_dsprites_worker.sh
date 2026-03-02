c4ea52f1e72e0189583f9bfc3e32f51fec91b4bc
Ahab Isaac <ahabisaac13@gmail.com>
2026-03-02 11:45:29 +0000

    dsprites config

--- experiments/run_dsprites_worker.sh
+++ experiments/run_dsprites_worker.sh
@@ -3,7 +3,7 @@
 #$ -o ~/Scratch/xai-crcbm/logs/dsprites_$JOB_ID.out
 #$ -e ~/Scratch/xai-crcbm/logs/dsprites_$JOB_ID.err
 #$ -pe smp 8
-#$ -l h_rt=18:00:00
+#$ -l h_rt=4:00:00
 #$ -l mem=8G
 #$ -l tmpfs=20G
 #$ -wd /home/ucakais/Scratch/xai-crcbm
