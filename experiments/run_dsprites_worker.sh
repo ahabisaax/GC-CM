9034fa5ed5d7440e155443bb2773b87bf4054d7c
Ahab Isaac <ahabisaac13@gmail.com>
2026-03-24 09:58:16 +0000

    sequential cbm config for dsprites

--- experiments/run_dsprites_worker.sh
+++ experiments/run_dsprites_worker.sh
@@ -3,7 +3,7 @@
 #$ -o ~/Scratch/xai-crcbm/logs/dsprites_$JOB_ID.out
 #$ -e ~/Scratch/xai-crcbm/logs/dsprites_$JOB_ID.err
 #$ -pe smp 8
-#$ -l h_rt=15:00:00
+#$ -l h_rt=18:00:00
 #$ -l mem=8G
 #$ -l tmpfs=20G
 #$ -wd /home/ucakais/Scratch/xai-crcbm
@@ -30,7 +30,7 @@ export MKL_NUM_THREADS=$NSLOTS
 # --- 3. PATHS ---
 PROJECT_ROOT=~/Scratch/xai-crcbm
 #CHANGE FOR EACH DATASET
-RESULTS_DIR=$PROJECT_ROOT/results/dsprites_ACBM_shared_critic
+RESULTS_DIR=$PROJECT_ROOT/results/dsprites_sequential_acbm
 CONFIG_PATH=$PROJECT_ROOT/experiments/configs/dsprites.yaml
 
 export PYTHONPATH=$PROJECT_ROOT:$PYTHONPATH
